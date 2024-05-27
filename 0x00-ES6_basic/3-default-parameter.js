21c8e824cae65afac28cb8ba09f288cc33aa6f0f
Nickson K Rop <nickyrutto@gmail.com>
2024-05-27 09:33:45 -0700

    condensing a function using arrow functions

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,1 +1,2 @@
-export default (initialNumber, expansion1989=89, expansion2019=19) => initialNumber + expansion1989 + expansion2019;
+/* eslint-disable max-len */
+export default (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
