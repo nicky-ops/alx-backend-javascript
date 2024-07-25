11d5efedc12130bc77c8aaf4abfbf8d6eaa94a92
Nickson K Rop <nickyrutto@gmail.com>
2024-07-25 05:23:26 +0000

    async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.deep.equal({data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+
+  it('should do nothing when success is false', (done) => {
+    const result = getPaymentTokenFromAPI(false);
+    expect(result).to.be.undefined;
+    done();
+  });
+});
